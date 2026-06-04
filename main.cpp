4536cdf001244496f7465cf5fd6e9fb1fcea3fc8
qu1z3x <dav-agaf@yandex.ru>
2026-06-04 13:12:16 +0300

    Final version

--- main.cpp
+++ main.cpp
@@ -1,28 +1,39 @@
-/*
-**************
-	ФИО: Агафонов Давид
-	Вариант: 1
-	(остальное палить на паблик репо не буду)
-**************
-*/
+/******************************************************************************
+ *                      КАФЕДРА №304 1 КУРС ПРОГИНЖ                            *
+ *                           Летняя Практика                                   *
+ *-----------------------------------------------------------------------------*
+ * Project Type  : macOS Console Application                                   *
+ * Project Name  : mai_practice                                                *
+ * File Name     : main.cpp                                                    *
+ * Language      : C/C++                                                       *
+ * Programmer    : Агафонов Давид                                              *
+ * Modified By   :                                                             *
+ * Created       : 26/05/2026                                                  *
+ * Comment(s)    : Работа со структурами и индексной сортировкой               *
+ ******************************************************************************/
 
 #include <cctype>
 #include <cstdio>
 #include <cstring>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
 
 using namespace std;
 
-const char *fileName = "planes.txt";
+const char DEFAULT_FILE[] = "planes.txt";			// файл по умолчанию
+const char POSITIVE_FILE[] = "planes_positive.txt"; // хорошие данные
+const char NEGATIVE_FILE[] = "planes_negative.txt"; // плохие данные
+const char MIXED_FILE[] = "planes_mixed.txt";		// хорошие и плохие данные
+const char BOUNDARY_FILE[] = "planes_boundary.txt"; // проверки границ
 
-const int MAX_PLANES = 100;
+const int MAX_PLANES = 100; // максимум записей для динамического массива
+const int MAX_ERRORS = 100;
+const int ERROR_SIZE = 320;
 const int LINE_SIZE = 256;
 const int TIME_SIZE = 6;
 const int MODEL_SIZE = 32;
 const int BOARD_SIZE = 16;
-const int CITY_SIZE = 32;
+const int CITY_SIZE = 64;
 
 struct Plane
 {
@@ -30,38 +41,50 @@ struct Plane
 	int landingMinutes;
 	char model[MODEL_SIZE];
 	char boardNumber[BOARD_SIZE];
-	char departure[CITY_SIZE];
+	char city[CITY_SIZE];
 };
 
-// Проверяет пустую строку; line - строка из файла.
+/****************************************************************/
+/*          П Р О Т О Т И П Ы     Ф У Н К Ц И Й                 */
+/****************************************************************/
+
+// проверяет пустую строку, line - строка из файла
 bool isEmptyLine(const char line[]);
 
-// Проверяет время; text - время, minutes - результат в минутах.
+// проверяет цифру, ch - один символ
+bool isDigit(char ch);
+
+// проверяет время, text - время, minutes - результат в минутах
 bool parseTime(const char text[], int &minutes);
 
-// Проверяет борт; text - бортовой номер.
+// проверяет борт, text - бортовой номер
 bool isGoodBoardNumber(const char text[]);
 
-// Разбирает строку; line - строка, plane - запись, error - текст ошибки.
+// разбирает строку, line - строка, plane - запись, error - текст ошибки
 bool parsePlane(const char line[], Plane &plane, const char *&error);
 
-// Проверяет борт и модель; planes - массив, count - размер, plane - новая запись.
+// проверяет борт и модель, planes - массив, count - размер, plane - новая запись
 bool checkPlaneLogic(const Plane planes[], int count, const Plane &plane, const char *&error);
 
-// Читает файл; fileName - имя файла, planes - массив, count - число записей.
-bool loadPlanes(const char fileName[], Plane planes[], int &count);
+// добавляет ошибку, errors - список, lineNumber - номер строки
+void addError(char errors[][ERROR_SIZE], int &errorCount, int lineNumber, const char line[], const char message[]);
 
-// Заполняет индексы; indexes - массив индексов, count - число записей.
-void fillIndexes(int indexes[], int count);
+// читает файл, fileName - имя, planes - записи, errors - список ошибок
+bool loadPlanes(const char fileName[], Plane planes[], int &count, char errors[][ERROR_SIZE], int &errorCount);
 
-// Сортирует индексы; planes - записи, indexes - индексы, count - число записей.
+// сортирует индексы, planes - записи, indexes - индексы, count - число записей
 void sortIndexesByTime(const Plane planes[], int indexes[], int count);
 
-// Печатает таблицу; planes - записи, indexes - порядок вывода, count - число записей.
+// печатает таблицу, planes - записи, indexes - порядок вывода, count - число записей
 void printTable(const Plane planes[], const int indexes[], int count);
+
+/****************************************************************/
+/*             О С Н О В Н А Я     П Р О Г Р А М М А            */
+/****************************************************************/
 
 int main(int argc, char *argv[])
 {
+	const char *fileName = DEFAULT_FILE;
 	if (argc > 1)
 	{
 		fileName = argv[1];
@@ -69,16 +92,37 @@ int main(int argc, char *argv[])
 
 	Plane *planes = new Plane[MAX_PLANES];
 	int *indexes = new int[MAX_PLANES];
+	char errors[MAX_ERRORS][ERROR_SIZE];
 	int count = 0;
+	int errorCount = 0;
 	int result = 0;
 
-	if (!loadPlanes(fileName, planes, count))
+	if (!loadPlanes(fileName, planes, count, errors, errorCount))
 	{
 		result = 1;
 	}
+
+	if (errorCount > 0)
+	{
+		cout << "\nОшибки:\n";
+		for (int i = 0; i < errorCount; i++)
+		{
+			cout << "- " << errors[i] << "\n";
+		}
+	}
+
+	if (count == 0)
+	{
+		cout << "\nКорректных записей для таблицы нет\n";
+	}
 	else
 	{
-		fillIndexes(indexes, count);
+		// заполняем индексы перед сортировкой
+		for (int i = 0; i < count; i++)
+		{
+			indexes[i] = i;
+		}
+
 		sortIndexesByTime(planes, indexes, count);
 		printTable(planes, indexes, count);
 	}
@@ -102,6 +146,11 @@ bool isEmptyLine(const char line[])
 	return true;
 }
 
+bool isDigit(char ch)
+{
+	return ch >= '0' && ch <= '9';
+}
+
 bool parseTime(const char text[], int &minutes)
 {
 	if (strlen(text) != 5 || text[2] != ':')
@@ -109,8 +158,8 @@ bool parseTime(const char text[], int &minutes)
 		return false;
 	}
 
-	if (!isdigit((unsigned char)text[0]) || !isdigit((unsigned char)text[1]) ||
-		!isdigit((unsigned char)text[3]) || !isdigit((unsigned char)text[4]))
+	if (!isDigit(text[0]) || !isDigit(text[1]) ||
+		!isDigit(text[3]) || !isDigit(text[4]))
 	{
 		return false;
 	}
@@ -129,7 +178,7 @@ bool parseTime(const char text[], int &minutes)
 
 bool isGoodBoardNumber(const char text[])
 {
-	// Буква "Б" в UTF-8 хранится двумя байтами: D0 91.
+	// буква Б в UTF-8 хранится двумя байтами
 	unsigned char first = (unsigned char)text[0];
 	unsigned char second = (unsigned char)text[1];
 
@@ -140,7 +189,7 @@ bool isGoodBoardNumber(const char text[])
 
 	for (int i = 3; i < 7; i++)
 	{
-		if (!isdigit((unsigned char)text[i]))
+		if (!isDigit(text[i]))
 		{
 			return false;
 		}
@@ -151,47 +200,70 @@ bool isGoodBoardNumber(const char text[])
 
 bool parsePlane(const char line[], Plane &plane, const char *&error)
 {
-	char time[TIME_SIZE];
-	char model[MODEL_SIZE];
-	char boardNumber[BOARD_SIZE];
-	char departure[CITY_SIZE];
-	char extra[LINE_SIZE];
+	int position = 0;
 
-	int fields = sscanf(line, "%5s %31s %15s %31s %255s",
-						time, model, boardNumber, departure, extra);
+	int fields = sscanf(line, "%5s %31s %15s %n", plane.landingTime,
+						plane.model, plane.boardNumber, &position);
 
-	if (fields < 4)
+	if (fields < 3)
 	{
-		error = "нужно 4 поля: время марка борт пункт";
+		error = "нужно минимум 4 поля: время марка борт пункт";
 		return false;
 	}
 
-	if (fields > 4)
+	while (line[position] == ' ' || line[position] == '\t')
 	{
-		error = "лишние данные в конце строки";
+		position++;
+	}
+
+	if (line[position] == '\0')
+	{
+		error = "отсутствует пункт отправления";
+		return false;
+	}
+
+	if (strlen(line + position) >= CITY_SIZE)
+	{
+		error = "пункт отправления слишком длинный";
 		return false;
 	}
 
 	int minutes = 0;
-	if (!parseTime(time, minutes))
+	if (!parseTime(plane.landingTime, minutes))
 	{
 		error = "время должно быть в формате HH:MM от 00:00 до 23:59";
 		return false;
 	}
 
-	if (!isGoodBoardNumber(boardNumber))
+	if (!isGoodBoardNumber(plane.boardNumber))
 	{
 		error = "бортовой номер должен быть в формате Б-хххх";
 		return false;
 	}
 
-	strcpy(plane.landingTime, time);
 	plane.landingMinutes = minutes;
-	strcpy(plane.model, model);
-	strcpy(plane.boardNumber, boardNumber);
-	strcpy(plane.departure, departure);
+	strcpy(plane.city, line + position);
 
 	return true;
+}
+
+void addError(char errors[][ERROR_SIZE], int &errorCount, int lineNumber, const char line[], const char message[])
+{
+	if (errorCount >= MAX_ERRORS)
+	{
+		return;
+	}
+
+	if (lineNumber > 0)
+	{
+		snprintf(errors[errorCount], ERROR_SIZE, "Строка %d: %s | %s", lineNumber, message, line);
+	}
+	else
+	{
+		snprintf(errors[errorCount], ERROR_SIZE, "%s", message);
+	}
+
+	errorCount++;
 }
 
 bool checkPlaneLogic(const Plane planes[], int count, const Plane &plane, const char *&error)
@@ -218,13 +290,17 @@ bool checkPlaneLogic(const Plane planes[], int count, const Plane &plane, const 
 	return true;
 }
 
-bool loadPlanes(const char fileName[], Plane planes[], int &count)
+bool loadPlanes(const char fileName[], Plane planes[], int &count, char errors[][ERROR_SIZE], int &errorCount)
 {
 	ifstream file(fileName);
 
 	if (!file)
 	{
-		cout << "Ошибка: не удалось открыть файл " << fileName << "\n";
+		char message[ERROR_SIZE];
+		snprintf(message, ERROR_SIZE, "не удалось открыть файл %s", fileName);
+		addError(errors, errorCount, 0, "", message);
+		cout << "Файлы для проверки: " << DEFAULT_FILE << ", " << POSITIVE_FILE << ", "
+			 << NEGATIVE_FILE << ", " << MIXED_FILE << ", " << BOUNDARY_FILE << "\n";
 		return false;
 	}
 
@@ -232,20 +308,39 @@ bool loadPlanes(const char fileName[], Plane planes[], int &count)
 	int lineNumber = 0;
 	char line[LINE_SIZE];
 
-	while (file.getline(line, LINE_SIZE))
+	while (true)
 	{
+		file.getline(line, LINE_SIZE);
+
+		if (!file)
+		{
+			if (file.eof())
+			{
+				break;
+			}
+
+			if (file.fail())
+			{
+				lineNumber++;
+				addError(errors, errorCount, lineNumber, "", "слишком длинная строка");
+				file.clear();
+				file.ignore(10000, '\n');
+				continue;
+			}
+		}
+
 		lineNumber++;
 
 		if (isEmptyLine(line))
 		{
-			cout << "Ошибка в строке " << lineNumber << ": пустая строка\n";
-			return false;
+			addError(errors, errorCount, lineNumber, line, "пустая строка");
+			continue;
 		}
 
 		if (count == MAX_PLANES)
 		{
-			cout << "Ошибка: записей больше " << MAX_PLANES << "\n";
-			return false;
+			addError(errors, errorCount, lineNumber, line, "слишком много корректных записей");
+			continue;
 		}
 
 		Plane plane;
@@ -253,48 +348,35 @@ bool loadPlanes(const char fileName[], Plane planes[], int &count)
 
 		if (!parsePlane(line, plane, error))
 		{
-			cout << "Ошибка в строке " << lineNumber << ": " << error << "\n";
-			cout << "Строка: " << line << "\n";
-			return false;
+			addError(errors, errorCount, lineNumber, line, error);
+			continue;
 		}
 
 		if (!checkPlaneLogic(planes, count, plane, error))
 		{
-			cout << "Ошибка в строке " << lineNumber << ": " << error << "\n";
-			cout << "Строка: " << line << "\n";
-			return false;
+			addError(errors, errorCount, lineNumber, line, error);
+			continue;
 		}
 
 		planes[count] = plane;
 		count++;
 	}
 
-	if (file.fail() && !file.eof())
+	if (lineNumber == 0)
 	{
-		cout << "Ошибка: слишком длинная строка в файле\n";
-		return false;
+		addError(errors, errorCount, 0, "", "файл пустой");
 	}
-
-	if (count == 0)
+	else if (count == 0)
 	{
-		cout << "Ошибка: файл пустой\n";
-		return false;
+		addError(errors, errorCount, 0, "", "корректных записей в файле нет");
 	}
 
 	return true;
 }
 
-void fillIndexes(int indexes[], int count)
-{
-	for (int i = 0; i < count; i++)
-	{
-		indexes[i] = i;
-	}
-}
-
 void sortIndexesByTime(const Plane planes[], int indexes[], int count)
 {
-	// Сортируем индексы. Сами записи самолетов не трогаем.
+	// сортируем индексы, сами записи самолетов не трогаем
 	for (int i = 0; i < count - 1; i++)
 	{
 		int minIndex = i;
@@ -316,17 +398,18 @@ void sortIndexesByTime(const Plane planes[], int indexes[], int count)
 void printTable(const Plane planes[], const int indexes[], int count)
 {
 	cout << "\nСамолеты по возрастанию времени посадки:\n\n";
-	cout << "Время | Марка        | Борт         | Пункт отправления\n";
-	cout << "-------------------------------------------------------------\n";
+	cout << "Время | Марка | Борт | Пункт отправления\n";
+	cout << "-----------------------------------------\n";
 
 	for (int i = 0; i < count; i++)
 	{
 		const Plane &plane = planes[indexes[i]];
 
-		cout << left
-			 << setw(5) << plane.landingTime << " | "
-			 << setw(12) << plane.model << " | "
-			 << setw(12) << plane.boardNumber << " | "
-			 << plane.departure << "\n";
+		cout << plane.landingTime << " | "
+			 << plane.model << " | "
+			 << plane.boardNumber << " | "
+			 << plane.city << "\n";
 	}
 }
+
+/* -------------    КОНЕЦ ФАЙЛА main.cpp    ------------- */
